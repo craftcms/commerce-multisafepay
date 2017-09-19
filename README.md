8295121f0c64d83837d1a2bc2b1781047d6c999a
andris-sevcenko <asevcenko@gmail.com>
2017-09-19 16:03:04 +0300

    Get the gateway working.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# commerce-multisafepay
+MultiSafePay payment gateway for Craft Commerce
+=======================
+
+This plugin provides [MultiSafepay](https://www.multisafepay.com/) integration for [Craft Commerce](https://craftcommerce.com/).
+
+It provides the MultiSafepay REST gateway.
+
+## Requirements
+
+This plugin requires Craft Commerce 2.0.1-alpha.4 or later.
+
+## Installation
+
+To install the plugin, follow these instructions.
+
+1. Open your terminal and go to your Craft project:
+
+        cd /path/to/project
+
+2. Then tell Composer to load the plugin:
+
+        composer require craftcms/commerce-multisafepay
+
+3. In the Control Panel, go to Settings → Plugins and click the “Install” button for MultiSafepay.
+
+## Setup
+
+To add a MultiSafepay payment gateway, go to Commerce → Settings → Gateways, create a new gateway, and set the gateway type to “MultiSafepay REST”.
---
+++ src/gateways/Gateway.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace craft\commerce\multisafepay\gateways;
+
+use Craft;
+use craft\commerce\base\RequestResponseInterface;
+use craft\commerce\models\payments\BasePaymentForm;
+use craft\commerce\models\Transaction;
+use craft\commerce\omnipay\base\OffsiteGateway;
+use craft\helpers\Json;
+use Omnipay\Common\AbstractGateway;
+use Omnipay\Common\Message\RequestInterface;
+use Omnipay\MultiSafepay\Message\RestRefundRequest;
+use Omnipay\Omnipay;
+use Omnipay\MultiSafepay\RestGateway as OmnipayGateway;
+use yii\base\NotSupportedException;
+
+/**
+ * PayPalPro represents PayPalPro gateway
+ *
+ * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since     1.0
+ */
+class Gateway extends OffsiteGateway
+{
+    // Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $apiKey;
+
+    /**
+     * @var bool
+     */
+    public $testMode = false;
+
+    /**
+     * @var string
+     */
+    public $locale;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('commerce', 'MultiSafepay REST');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSettingsHtml()
+    {
+        return Craft::$app->getView()->renderTemplate('commerce-multisafepay/gatewaySettings', ['gateway' => $this]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function populateRequest(array &$request, BasePaymentForm $paymentForm = null)
+    {
+        parent::populateRequest($request, $paymentForm);
+        $request['type'] = 'redirect';
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected function createGateway(): AbstractGateway
+    {
+        /** @var OmnipayGateway $gateway */
+        $gateway = Omnipay::create($this->getGatewayClassName());
+
+        $gateway->setApiKey($this->apiKey);
+        $gateway->setLocale($this->locale);
+        $gateway->setTestMode($this->testMode);
+
+        return $gateway;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getGatewayClassName()
+    {
+        return '\\'.OmnipayGateway::class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function refund(Transaction $transaction, string $reference): RequestResponseInterface
+    {
+
+        $request = $this->createRequest($transaction);
+        $refundRequest = $this->prepareRefundRequest($request, $reference);
+
+        // Get the order ID for the successful transaction and use that.
+        $responseData = Json::decodeIfJson($transaction->getParent()->response);
+
+        if ($responseData && isset($responseData['data']['order_id'])) {
+            $reference = $responseData['data']['order_id'];
+        } else {
+            throw new NotSupportedException('Cannot refund this transaction as the parent Order cannot be found!');
+        }
+
+        /** @var RestRefundRequest $refundRequest */
+        $refundRequest->setTransactionId($reference);
+
+        return $this->performRequest($refundRequest, $transaction);
+    }
+}
